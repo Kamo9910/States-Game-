3013e2f58d9bdc2915ac6d5a930bd3cb249a71b7
Danny Kamohelo Ntshala <ntshaladannykamohelo@gmail.com>
2025-10-30 16:25:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+from turtle import Screen
+import pandas
+
+screen = Screen()
+
+image = "C:/Users/Ntshala Empire/PycharmProjects/PythonProject21/blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.exitonclick()
